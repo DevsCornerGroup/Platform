0b377798afc3354dda4d23fde7c0e7f5acaf2575
Yoginth <yoginth@aol.com>
2018-03-02 16:04:01 +0000

    Updated votepen-email-header to CDN

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="https://votepen.com/imgs/votepen-email-header.png" width="180" alt="VotePen">
+            <img src="https://cdn.jsdelivr.net/npm/votepen-cdn@1.0.0-beta/imgs/votepen-email-header.png" width="180" alt="VotePen">
         </a>
     </td>
 </tr>
