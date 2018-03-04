e195c7b3ebe1975d09af5497a2f21d9fed828cf2
Yoginth <yoginth@aol.com>
2018-03-04 09:04:11 +0000

    Updated Email Header image

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="{{ config('app.cdn_url') }}imgs/votepen-email-header.png" width="180" alt="VotePen">
+            <img src="{{ config('app.cdn_url') }}imgs/votepen-email-header.png" width="100" alt="VotePen">
         </a>
     </td>
 </tr>
