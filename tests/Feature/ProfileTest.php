6eff4d1bb2370051cdbb53a72df0f3541d13b80b
Yoginth <yoginth@aol.com>
2018-03-06 14:32:21 +0000

    Make Test case Pass

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -3,26 +3,28 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProfileTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase; 
 
     /** @test */
     public function profile_page_displays_correct_info()
     {
         create('App\User', [
-            'username' => 'YoginthS',
-            'name'     => 'Yoginth',
-            'location' => 'Earth',
-            'info'     => [
-                'twitter' => 'yoginth_on_twitter',
-                'website' => 'https://votepen.com',
-            ],
+            'username' => 'YoginthS', 
+            'name' => 'Yoginth', 
+            'location' => 'Earth', 
+            'info' => [
+                'twitter' => 'yoginth_on_twitter', 
+                'website' => 'https://votepen.com'
+            ]
         ]);
 
-        $this->get('/@'.'YoginthS')
-            ->assertSee('@'.'YoginthS')
+        $this->get('/@' . 'YoginthS')
+            ->assertSee('@' . 'YoginthS')
             ->assertSeeText('Earth')
             ->assertSee('yoginth_on_twitter')
             ->assertSee('votepen.com')
