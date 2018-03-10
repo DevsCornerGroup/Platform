206fbeafcac9fc2f46d54c01d8df8860e5906422
Yoginth <yoginth@aol.com>
2018-03-10 15:24:14 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/PhotoTools.php
+++ app/PhotoTools.php
@@ -2,10 +2,6 @@
 
 namespace App;
 
-use Auth;
-use App\User;
-use App\Photo;
-use App\Category;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManagerStatic as Image;
 
@@ -16,17 +12,16 @@ trait PhotoTools
         return env('CDN_URL', 'https://i1.wp.com/votepen.tk');
     }
 
-
     /**
-     * Downloads the image from the external link and stores into the defined direcory( could be ftp, local...)
+     * Downloads the image from the external link and stores into the defined direcory( could be ftp, local...).
      *
      * [@param](https://votepen.com/@param)  (string) external url
      * [@param](https://votepen.com/@param)  (string) directory the file should be uploaded to
      * [@return](https://votepen.com/@return) (string) the path of uploaded file
      */
-    protected function downloadImg($url, $folder = "submissions/link")
+    protected function downloadImg($url, $folder = 'submissions/link')
     {
-        $filename = time() . str_random(7) . '.jpg';
+        $filename = time().str_random(7).'.jpg';
         $image = Image::make($url);
 
         if ($image->filesize() > 300000) { // 300kb
@@ -35,13 +30,13 @@ trait PhotoTools
             $image->encode('jpg', 90);
         }
 
-        Storage::put($folder . '/' . $filename, $image);
-        return $this->ftpAddress() . $folder . "/" . $filename;
+        Storage::put($folder.'/'.$filename, $image);
+
+        return $this->ftpAddress().$folder.'/'.$filename;
     }
 
-
     /**
-     * Creates and saves a thumbnail for the sent photo and stores into the defined direcory( could be ftp, local...)
+     * Creates and saves a thumbnail for the sent photo and stores into the defined direcory( could be ftp, local...).
      *
      * [@param](https://votepen.com/@param)  request('img')
      * [@param](https://votepen.com/@param)  (int) width of the thumbnail
@@ -49,9 +44,9 @@ trait PhotoTools
      * [@param](https://votepen.com/@param)  (string) directory the file should be uploaded to
      * [@return](https://votepen.com/@return) (string) the path of uploaded file
      */
-    protected function createThumbnail($url, $width, $height, $folder = "submissions/img/thumbs")
+    protected function createThumbnail($url, $width, $height, $folder = 'submissions/img/thumbs')
     {
-        $filename = time() . str_random(7) . '.jpg';
+        $filename = time().str_random(7).'.jpg';
         $image = Image::make($url);
 
         if ($image->width() > 1200) {
@@ -65,7 +60,8 @@ trait PhotoTools
         }
 
         $image->encode();
-        Storage::put($folder . '/' . $filename, $image);
-        return $this->ftpAddress() . $folder . "/" . $filename;
+        Storage::put($folder.'/'.$filename, $image);
+
+        return $this->ftpAddress().$folder.'/'.$filename;
     }
 }
