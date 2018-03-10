e3553c6d24516df591a551d178869e5adf2b8146
Yoginth <yoginth@zoho.com>
2018-03-10 15:24:05 +0000

    Updated Photo Tools Trait

--- app/PhotoTools.php
+++ app/PhotoTools.php
@@ -2,29 +2,56 @@
 
 namespace App;
 
+use Auth;
+use App\User;
+use App\Photo;
+use App\Category;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManagerStatic as Image;
 
 trait PhotoTools
 {
-    protected function webAddress()
+    protected function ftpAddress()
     {
-        return config('filesystems.cdn_url');
+        return env('CDN_URL', 'https://i1.wp.com/votepen.tk');
     }
 
+
     /**
-     * Creates and saves a thumbnail for the sent photo and stores into the defined direcory( could be ftp, local...).
+     * Downloads the image from the external link and stores into the defined direcory( could be ftp, local...)
      *
-     * @param  request('img')
-     * @param  (int) width of the thumbnail
-     * @param  (int) height of the thumbnail
-     * @param  (string) directory the file should be uploaded to
-     *
-     * @return (string) the path of uploaded file
+     * [@param](https://votepen.com/@param)  (string) external url
+     * [@param](https://votepen.com/@param)  (string) directory the file should be uploaded to
+     * [@return](https://votepen.com/@return) (string) the path of uploaded file
      */
-    protected function createThumbnail($url, $width, $height, $folder = 'submissions/img/thumbs')
+    protected function downloadImg($url, $folder = "submissions/link")
     {
-        $filename = time().str_random(16).'.jpg';
+        $filename = time() . str_random(7) . '.jpg';
+        $image = Image::make($url);
+
+        if ($image->filesize() > 300000) { // 300kb
+            $image->encode('jpg', 60);
+        } else {
+            $image->encode('jpg', 90);
+        }
+
+        Storage::put($folder . '/' . $filename, $image);
+        return $this->ftpAddress() . $folder . "/" . $filename;
+    }
+
+
+    /**
+     * Creates and saves a thumbnail for the sent photo and stores into the defined direcory( could be ftp, local...)
+     *
+     * [@param](https://votepen.com/@param)  request('img')
+     * [@param](https://votepen.com/@param)  (int) width of the thumbnail
+     * [@param](https://votepen.com/@param)  (int) height of the thumbnail
+     * [@param](https://votepen.com/@param)  (string) directory the file should be uploaded to
+     * [@return](https://votepen.com/@return) (string) the path of uploaded file
+     */
+    protected function createThumbnail($url, $width, $height, $folder = "submissions/img/thumbs")
+    {
+        $filename = time() . str_random(7) . '.jpg';
         $image = Image::make($url);
 
         if ($image->width() > 1200) {
@@ -38,114 +65,7 @@ trait PhotoTools
         }
 
         $image->encode();
-        Storage::put($folder.'/'.$filename, $image);
-
-        return $this->webAddress().$folder.'/'.$filename;
-    }
-
-    /**
-     * Crops the uploaded photo with the sent coordinates.
-     *
-     * @param string $url
-     * @param int    $width
-     * @param int    $height
-     * @param int    $x
-     * @param int    $y
-     * @param string $folder
-     *
-     * @return string
-     */
-    protected function cropImg($url, $width, $height, $x, $y, $folder = 'users/avatars')
-    {
-        $filename = time().str_random(16).'.png';
-        $image = Image::make($url);
-        $image = $image->crop($width, $height, $x, $y);
-
-        // optimize the croped image (decrease filesize)
-        $image = $image->resize(250, 250);
-
-        if ($image->filesize() > 50000) { // 50kb
-            $image->encode('png', 60);
-        } else {
-            $image->encode('png', 90);
-        }
-
-        Storage::put($folder.'/'.$filename, $image);
-
-        return $this->webAddress().$folder.'/'.$filename;
-    }
-
-    /**
-     * Uplaods the file into the defined direcory( could be ftp, local...). Encodes all the formates
-     * into jpg, also optimizes it so the uploaded photos will have less size. In case the image is
-     * in .gif format, it doesn't touch it. Just uploads the file while keeping the .gif format.
-     *
-     * @param  request('img')
-     * @param  (string) directory the file should be uploaded to
-     *
-     * @return (string) the path of uploaded file
-     */
-    protected function uploadImg($image, $folder = 'submissions/img')
-    {
-        $filename = time().str_random(16).'.jpg';
-        $image = Image::make($image->getRealPath());
-
-        if (!$image->filesize()) {
-            return;
-        }
-
-        if ($image->filesize() > 300000) {
-            $image->encode('jpg', 60);
-        } else {
-            $image->encode('jpg', 90);
-        }
-
-        Storage::put($folder.'/'.$filename, $image);
-
-        return $this->webAddress().$folder.'/'.$filename;
-    }
-
-    /**
-     * Uplaods the file into the defined direcory( could be ftp, local...).
-     *
-     * @param  request('img')
-     * @param  (string) directory the file should be uploaded to
-     *
-     * @return (string) the path of uploaded file
-     */
-    protected function uploadImgPNG($image, $folder = 'submissions/img')
-    {
-        $filename = time().str_random(16).'.png';
-        $image = Image::make($image->getRealPath());
-
-        $image->encode('png');
-
-        Storage::put($folder.'/'.$filename, $image);
-
-        return $this->webAddress().$folder.'/'.$filename;
-    }
-
-    /**
-     * Downloads the image from the external link and stores into the defined direcory( could be ftp, local...).
-     *
-     * @param  (string) external url
-     * @param  (string) directory the file should be uploaded to
-     *
-     * @return (string) the path of uploaded file
-     */
-    protected function downloadImg($url, $folder = 'submissions/link')
-    {
-        $filename = time().str_random(16).'.jpg';
-        $image = Image::make($url);
-
-        if ($image->filesize() > 300000) { // 300kb
-            $image->encode('jpg', 60);
-        } else {
-            $image->encode('jpg', 90);
-        }
-
-        Storage::put($folder.'/'.$filename, $image);
-
-        return $this->webAddress().$folder.'/'.$filename;
+        Storage::put($folder . '/' . $filename, $image);
+        return $this->ftpAddress() . $folder . "/" . $filename;
     }
 }
